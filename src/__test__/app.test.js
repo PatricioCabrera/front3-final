4761a5e1d6c79d95566f93a3b5e7e0e2861ac18a
rafoonr <rafaelrodriguezq11@gmail.com>
2023-07-01 01:43:56 -0300

    ultimocomitcreo

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Card from '../Components/Card';
 import { ContextProvider } from '../Components/utils/global.context';
+import Home from '../Routes/Home';
 
-test('renders Card component', () => {
-  const mockData = [
-    { id: 1, name: 'Leanne Graham', username: 'Bret' },
-    { id: 2, name: 'Ervin Howell', username: 'Antonette' },
-  ];
-
+test('renders Home component', () => {
   const { getByText } = render(
     <ContextProvider>
-      <Card data={mockData} />
+      <Home />
     </ContextProvider>
   );
 
-  const dentistName1 = getByText(/Leanne Graham/i);
-  const dentistName2 = getByText(/Ervin Howell/i);
+  // Buscar el texto "Home" dentro del componente Navbar
+  const homeText = getByText(/Home/, { selector: 'a' });
 
-  expect(dentistName1).toBeInTheDocument();
-  expect(dentistName2).toBeInTheDocument();
+  expect(homeText).toBeInTheDocument();
 });
